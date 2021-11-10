5df5212b2888047339b236bd7246396ba4c5c593
Laurens van der Bilt <laurensvanderbilt@pop-os.localdomain>
2021-11-10 21:20:36 +0100

    Changed styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,20 +18,37 @@
             font-size: 200%;
             color: green;
             font-weight: bold;
+            max-width: 50%;
         }
     </style>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
 <canvas id="canvas"></canvas>
-<main>
+    <main class="p-12">
     <header>
         <h1>Laurens van der Bilt</h1>
-        <p>🚀 Welcome to my website!</p>
-        <div class="flex">
-            
-        </div>
     </header>
-</main>
+    <p>I am a learning programmer living in Amsterdam and studying at the bit-academy.
+        My hobbies include programming, gaming and hanging out with friends.</p>
+    <br>
+    <p>I built this with Laravel and THREE.js</p>
+    <div class="flex justify-start">
+        <div class="justify-around">
+            <a href="https://github.com/2099492">
+                <x-button class="bg-blue-900">GitHub</x-button>
+            </a>
+            <a href="https://www.linkedin.com/in/laurens-van-der-bilt/">
+                <x-button class="bg-blue-900">LinkedIn</x-button>
+            </a>
+        </div>
+    </div>
+    </main>
 <script src="/js/three.js" type="module"></script>
 </body>
 </html>
