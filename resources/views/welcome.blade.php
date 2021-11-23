1dd2f6c006b6f961906300fdc12f786b9b2ce6bc
Laurens van der Bilt <laurensvanderbilt@pop-os.localdomain>
2021-11-23 15:27:41 +0100

    Added picture

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,23 +37,28 @@
 <body>
 <canvas id="canvas"></canvas>
 <main class="p-12 flex-col justify-start sm:w-2/3">
-<header>
-    <h1>Hi, my name is Laurens van der Bilt</h1>
-</header>
-<p>I am a learning programmer living in Amsterdam and studying at the Bit Academy.
-    My hobbies include programming, gaming and hanging out with friends.</p>
-<br>
-<p>I built this with Laravel and THREE.js</p>
-<div class="">
-    <div class="justify-around">
-        <a href="https://github.com/2099492">
-            <x-button class="bg-blue-900">GitHub</x-button>
-        </a>
-        <a href="https://www.linkedin.com/in/laurens-van-der-bilt/">
-            <x-button class="bg-blue-900">LinkedIn</x-button>
-        </a>
+    <div class="flex">
+        <div class="flex-none">
+            <img style="height: 200px;" class="p-3 rounded" src="/images/IMG_3084.jpeg" alt="">
+        </div>
+        <div class="flex-grow">
+            <h1>Hi, my name is Laurens van der Bilt</h1>
+            <p>I am a learning programmer living in Amsterdam and studying at the Bit Academy.
+                My hobbies include programming, gaming and hanging out with friends.</p>
+            <br>
+            <p>I built this with Laravel and THREE.js</p>
+        </div>
     </div>
-</div>
+    <div class="">
+        <div class="justify-around">
+            <a href="https://github.com/2099492">
+                <x-button class="bg-blue-900">GitHub</x-button>
+            </a>
+            <a href="https://www.linkedin.com/in/laurens-van-der-bilt/">
+                <x-button class="bg-blue-900">LinkedIn</x-button>
+            </a>
+        </div>
+    </div>
 </main>
 <script src="/js/three.js" type="module"></script>
 </body>
